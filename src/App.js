6d6359d3cb22f5fd5bd626608543a6b2caabd3b0
Willian Mews <wmews@paypal.com>
2020-02-04 21:26:47 -0800

    update content for new commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Documentation Project: {content.html}
+          Documentation framework [tag 1.0.1]: {content.html}
         </p>
       </header>
     </div>
