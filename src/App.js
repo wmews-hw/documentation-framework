eecba0c77502c1889950bbdcc0ff39ef37fd94b8
Willian Mews <wmews@paypal.com>
2020-02-04 20:48:23 -0800

    adding react files

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+// import {fs} from 'fs';
+
+const content = require("../node_modules/documentation/src/content.json");
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Documentation Project: {content.html}
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
